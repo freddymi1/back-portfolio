2ee8ba8aee3f12712b2a468060fd54d87f62214d
freddymi1 <michelfreddy19922gmail.com>
2021-07-06 11:39:52 +0300

    📦 NEW: Create server

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ const express = require("express")
 
 const app = express()
 
-app.get("/api", req, res)
+app.get("/api", (req, res)=>{
+    res.json({
+        success: 1,
+        message: "Api rest in nodejs"
+    })
+});
+
+app.listen(5000, ()=>{
+    console.log("Server running on port 5000")
+})
